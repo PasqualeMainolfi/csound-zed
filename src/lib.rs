a5a9edf1381328324a46a5971d227125bdecb48e
PasqualeMainolfi <mnlpql@gmail.com>
2025-12-01 00:23:18 +0100

    start time zero lsp

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,24 @@ impl zed::Extension for CsoundExtension {
             Self: Sized {
                 Self
     }
+
+    fn language_server_command(
+        &mut self,
+        _language_server_id: &zed::LanguageServerId,
+        _worktree: &zed::Worktree
+    ) -> zed::Result<zed::Command> {
+
+
+        eprintln!("Try sto start server!");
+        let lsp_path = "/Users/pm/AcaHub/Coding/tree-sitter-csound/csound-lsp/target/release/csound-lsp";
+
+        Ok(zed::Command {
+            command: lsp_path.to_string(),
+            args: vec![],
+            env: Default::default()
+        })
+    }
+
 }
 
 zed::register_extension!(CsoundExtension);
